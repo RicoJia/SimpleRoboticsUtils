5bd16c6878ac9455fbe65b925bd19cfaa56316b9
ricocoyeah <rjia@diligentrobots.com>
2023-11-05 21:50:12 -0600

    tidied up pip pkg

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='simple_robotics_python_utils',
     version='0.1.0',
-    packages=find_packages(include=['simple_robotics_python_utils', 'simple_robotics_python_utils.*'])
+    packages=find_packages(include=['simple_robotics_python_utils', 'simple_robotics_python_utils.*']),
     install_requires=[
         "posix_ipc",
-    ]
+    ],
 )
 
 
