3a674a6113d323d45bd3651615dca1e986b58764
ricocoyeah <rjia@diligentrobots.com>
2023-12-09 19:56:41 -0600

    added io.py

--- simple_robotics_python_utils/common/io.py
+++ simple_robotics_python_utils/common/io.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 import os
+
+
 def try_remove_file(path):
     try:
         os.remove(path)
--- simple_robotics_python_utils/common/logger.py
+++ simple_robotics_python_utils/common/logger.py
@@ -3,15 +3,15 @@ import logging
 
 
 class ColorCode:
-    RED = '\033[91m'
-    DARK_RED = '\033[31m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    BLUE = '\033[94m'
-    MAGENTA = '\033[95m'
-    CYAN = '\033[96m'
-    WHITE = '\033[97m'
-    RESET = '\033[0m'
+    RED = "\033[91m"
+    DARK_RED = "\033[31m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    BLUE = "\033[94m"
+    MAGENTA = "\033[95m"
+    CYAN = "\033[96m"
+    WHITE = "\033[97m"
+    RESET = "\033[0m"
 
 
 class ColorFormatter(logging.Formatter):
@@ -20,12 +20,14 @@ class ColorFormatter(logging.Formatter):
         logging.INFO: ColorCode.GREEN,
         logging.WARNING: ColorCode.YELLOW,
         logging.ERROR: ColorCode.RED,
-        logging.CRITICAL: ColorCode.DARK_RED
+        logging.CRITICAL: ColorCode.DARK_RED,
     }
 
     def format(self, record):
-        record.name = f'{ColorCode.CYAN}{record.name}{ColorCode.RESET}'
-        record.levelname = f'{self.COLOR_MAP[record.levelno]}{record.levelname}{ColorCode.RESET}'
+        record.name = f"{ColorCode.CYAN}{record.name}{ColorCode.RESET}"
+        record.levelname = (
+            f"{self.COLOR_MAP[record.levelno]}{record.levelname}{ColorCode.RESET}"
+        )
         msg = logging.Formatter.format(self, record)
         parts = msg.split(" - ", maxsplit=1)
         parts[0] = f"{ColorCode.MAGENTA}{parts[0]}{ColorCode.RESET}"
@@ -42,7 +44,9 @@ def get_logger(name: str, print_level: str = "INFO"):
         logger.setLevel(level)
         console_handler = logging.StreamHandler()
         console_handler.setLevel(level)
-        formatter = ColorFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = ColorFormatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
         console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
     return logger
