d14bb85f94fb2d9aa106cd5e223e7a360f5452d5
ricocoyeah <rjia@diligentrobots.com>
2023-12-17 12:05:16 -0600

    tests test shm pub sub

--- simple_robotics_python_utils/tests/test_shm_pub_sub.py
+++ simple_robotics_python_utils/tests/test_shm_pub_sub.py
@@ -7,9 +7,8 @@ from multiprocessing import Process, Value
 import time
 
 
-# Need to
 class TestSharedMemoryPubSub:
-    def test_shm_pub_sub(self):
+    def _test_shm_pub_sub(self, use_ros):
         """
         spawn sub at 50hz in a new process
         Spawn pub in a new process
@@ -17,14 +16,18 @@ class TestSharedMemoryPubSub:
         stop the sub and the pub
         """
         num_message_received = Value("i", 0)
-        TEST_FREQUENCY = 80
-        NUM_TEST_MSG = 100
+        TEST_FREQUENCY = 40
+        NUM_TEST_MSG = 1000
 
-        def subscriber_reading_process(num_message_received):
-            def increment_num_message_received(_):
+        def subscriber_reading_process(num_message_received, use_ros):
+            def increment_num_message_received(msg):
                 nonlocal num_message_received
+                assert int(msg[1]) == num_message_received.value
                 num_message_received.value += 1
 
+            if use_ros:
+                import rospy
+                rospy.init_node("ros_test_sub")
             shm_sub = SharedMemorySub(
                 topic="test",
                 data_type=float,
@@ -32,25 +35,29 @@ class TestSharedMemoryPubSub:
                 read_frequency=TEST_FREQUENCY,
                 # debug=True,
                 callback=increment_num_message_received,
+                use_ros=use_ros,
             )
-            time.sleep(int(NUM_TEST_MSG / TEST_FREQUENCY) + 1)
+            time.sleep(int(NUM_TEST_MSG / TEST_FREQUENCY) + 2.5)
 
-        def publisher_publishing_process():
+        def publisher_publishing_process(use_ros):
+            if use_ros:
+                import rospy
+                rospy.init_node("ros_test_pub")
             shm_pub = SharedMemoryPub(
                 topic="test",
                 data_type=float,
                 arr_size=2,
-                # debug=True
+                # debug=True,
+                use_ros=use_ros,
             )
             time.sleep(0.5)
             for i in range(NUM_TEST_MSG):
                 shm_pub.publish([i, i])
                 time.sleep(1 / TEST_FREQUENCY)
-
         reader_proc = Process(
-            target=subscriber_reading_process, args=(num_message_received,)
+            target=subscriber_reading_process, args=(num_message_received, use_ros)
         )
-        publisher_proc = Process(target=publisher_publishing_process, args=())
+        publisher_proc = Process(target=publisher_publishing_process, args=(use_ros,))
 
         reader_proc.start()
         publisher_proc.start()
@@ -58,3 +65,7 @@ class TestSharedMemoryPubSub:
         publisher_proc.join()
 
         assert num_message_received.value == NUM_TEST_MSG
+
+    def test_shm_pub_sub(self):
+        # self._test_shm_pub_sub(use_ros=False)
+        self._test_shm_pub_sub(use_ros=True)
