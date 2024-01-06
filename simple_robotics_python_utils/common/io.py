21c478acbb594cc504d8184ac2e9b3d8977cc318
ricocoyeah <rjia@diligentrobots.com>
2024-01-06 17:08:32 -0600

    common io updated

--- simple_robotics_python_utils/common/io.py
+++ simple_robotics_python_utils/common/io.py
@@ -1,5 +1,15 @@
 #! /usr/bin/env python3
 import os
+import typing
+
+
+def feature_flag_read(name: str) -> typing.Union[bool, str]:
+    flag_val: str = os.environ.get(name).lower()
+    BOOL_FLAGS = {"true": True, "false": False}
+    if flag_val in BOOL_FLAGS:
+        return BOOL_FLAGS[flag_val]
+    else:
+        return flag_val
 
 
 def try_remove_file(path):
