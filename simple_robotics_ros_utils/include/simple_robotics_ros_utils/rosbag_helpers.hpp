05ad0e149f0834cf939a55644384667637634fa7
Rico <ricoruotongjia@gmail.com>
2024-07-16 22:42:19 -0500

    added some cv_utils

--- simple_robotics_ros_utils/include/simple_robotics_ros_utils/rosbag_helpers.hpp
+++ simple_robotics_ros_utils/include/simple_robotics_ros_utils/rosbag_helpers.hpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <optional>
 #include <ros/ros.h>
+#include <ros/package.h>
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
 #include <unordered_map>
@@ -13,10 +14,10 @@ namespace SimpleRoboticsRosUtils {
 
 class BagParser {
 public:
-  BagParser(ros::NodeHandle nh, const std::string &bag_file);
+  BagParser(ros::NodeHandle nh, const std::string &package_name, const std::string &bag_file);
   ~BagParser();
   template <typename T>
-  void add_topic(const std::string topic, const bool &publish_after_reading);
+  bool add_topic(const std::string& topic);
 
   // If the topic previously have been declared to publish, it will be done
   // here.
@@ -31,30 +32,38 @@ private:
   std::shared_ptr<rosbag::View> view_;
   std::unordered_map<std::string, ros::Publisher> publishers_;
   std::unordered_map<std::string, rosbag::View::iterator> msg_iterators_;
-  rosbag::View::iterator general_msg_iterator_;
+    std::set<std::string> topics_;
 };
 
-inline BagParser::BagParser(ros::NodeHandle nh, const std::string &bag_file) {
-  std::cout << "Opening bag file: " << bag_file << std::endl;
-  bag_.open(bag_file, rosbag::bagmode::Read);
+inline BagParser::BagParser(ros::NodeHandle nh, const std::string &package_name, const std::string &bag_file) {
+  std::string package_path = ros::package::getPath(package_name);
+  if (package_path.empty()) 
+    throw std::runtime_error(package_path + "does not exist");
+  std::string full_bag_path = package_path + "/" + bag_file;
+  std::cout << "Opening bag file: " << full_bag_path << std::endl;
+  bag_.open(full_bag_path, rosbag::bagmode::Read);
   view_ = std::make_shared<rosbag::View>(bag_);
-  general_msg_iterator_ = view_->begin();
-  for (auto connection_info : view_->getConnections()) {
-    std::cout << "BagParser added topic: " << connection_info->topic
-              << std::endl;
-    msg_iterators_.insert(
-        std::make_pair(connection_info->topic, view_->begin()));
-  }
+for (const auto& c: view_->getConnections())
+    topics_.insert(c->topic);
 }
 inline BagParser::~BagParser() { bag_.close(); }
 
-// returning message pointer  TODO: when publish_after_reading is not set yet
+/**
+ * @brief add a topic to the bag parser so later it can be used for analysis
+ * 
+ * @tparam T : type of the topic
+ * @param topic : topic name
+ * @return true : if the topic could be found
+ * @return false : if the topic could not be found
+ */
 template <typename T>
-inline void BagParser::add_topic(const std::string topic,
-                                 const bool &publish_after_reading) {
-  if (publish_after_reading) {
-    publishers_.insert(std::make_pair(topic, nh_.advertise<T>(topic, 1)));
-  }
+inline bool BagParser::add_topic(const std::string& topic) {
+    if (topics_.find(topic) == topics_.end())
+        return false;
+    std::cout << "BagParser added topic: " << topic << std::endl;
+    msg_iterators_.insert(
+        std::make_pair(topic, view_->begin()));
+    return true;
 }
 
 // if topic is none, we will return the next topic by the receipt time.
@@ -74,8 +83,6 @@ boost::shared_ptr<T> BagParser::next(const std::optional<std::string> &topic) {
       }
     }
   }
-  // TODO
-  std::cout << "finished next" << std::endl;
   return nullptr;
 }
 
